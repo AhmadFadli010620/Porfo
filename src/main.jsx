4decd094d31bbd0f63b807e67da71ce96df80ec3
AHMAD010620 <ahmadfadli010620@.com>
2025-07-15 14:04:07 +0700

    Fix import case sensitivity for Vercel build

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Navbar from "./components/Navbar.jsx";
-import Footer from "./components/footer.jsx";
+import Footer from "./components/Footer.jsx";
 import "remixicon/fonts/remixicon.css";
 import PreLoader from "./components/Preloader.jsx";
 import "animate.css";
